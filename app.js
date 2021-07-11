8783910b0dce40754b1aebf8822e8b8a32f37b70
kav-gif <63546283+kav-gif@users.noreply.github.com>
2021-07-11 17:49:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("#btn");
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+
+function changeColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    h1.innerText=`rgb(${red},${green},${blue})`
+    body.style.backgroundColor = `rgb(${red},${green},${blue})`
+    
+    
+}
+btn.addEventListener('click',changeColor)
+
+
+
